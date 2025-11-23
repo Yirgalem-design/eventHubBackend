4911fed92c0b225b9c9c27c2008b4350f3112d27
Yirgat wolde <yirgat.w@gmail.com>
2025-11-22 19:55:42 -0500

    update .env and db.js file

--- db.js
+++ db.js
@@ -1,9 +1,20 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
-});
+const pool = new Pool(
+  process.env.DATABASE_URL
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false },
+      }
+    : {
+        user: process.env.DB_USER,
+        host: process.env.DB_HOST,
+        database: process.env.DB_DATABASE,
+        password: process.env.DB_PASSWORD,
+        port: process.env.DB_PORT,
+        ssl: process.env.DB_SSL === "true" ? { rejectUnauthorized: false } : false,
+      }
+);
 
 module.exports = pool;
